15e920c1b021bf4999fce17e46024bc5c849e58e
Rajat1agva <88077000+Rajat1agva@users.noreply.github.com>
2021-09-06 12:36:03 +0530

    Update main.c

--- 2. ET_Pcontrol/ET_Pcontrol/ET_Pcontrol/main.c
+++ 2. ET_Pcontrol/ET_Pcontrol/ET_Pcontrol/main.c
@@ -1,9 +1,9 @@
 /*
- * ET_Pcontrol.c
+ * DVT_PUMP.c
  *
  * Created: 7/22/2021 9:50:45 AM
- * Author : Deepender Kumar
- * Hardware : AVR128DA48 - MPRSS0001PG00001C - L298N - 3V PUMP - 2 12V Solenoid - ET_CUFF Tube
+ * Author : RAJAT MISHRA
+ * Hardware : AVR128DA48 - MPRSS0001PG00001C 
  */
 
 
@@ -14,42 +14,33 @@
 #include <util/delay.h>
 #include "I2C_0_AVR128DA64.h"
 #include "UART_1_AVR128DA64.h"
-#include "RTC_AVR128DA64.h"
 #include "MPRSS_sensor.h"
-void SQ_wave(void);
+#include <avr/interrupt.h>
 
 void TCA0_PWM_init(void);
 void P_control(void);
 void PWM_Condition(void);
-void intubation(int mode);
 void continous_intubation(void);
-void squarewave_intubation(void);
-void sinewave_intubation(void);
-void SQ_wave(void);
+void TCB_TIMER_init(void);
 
-#define continous 1
-#define squarewave 2
-#define sinewave 3
-#define prssr_vle_hgh 70
-#define prssr_vle_lw 20
 
 
 /* Pressure Sensor Variables */
 float current_pressure =0;
 
-bool square_h = true;
+
 
 /* P_Control Variables */
 float Kp_error = 0.0, Kp = 1.5;
 float Set_pressure  = 61;
 uint32_t dutyCycle = 0; // OUTPUT ON PA0
-
+uint16_t  valve1_timer=0;
 
 bool flag =  false;
 
 bool low_pressure_flag = true;
 bool sq_wave_up_press = false;
-unsigned long currentmillis = 0, temp;
+
 
 
 
@@ -57,44 +48,51 @@ unsigned long currentmillis = 0, temp;
 #define MAX_PRESSURE  62
 #define MIN_PRESSURE  0
 
-#define leakage_limit  50
-float current_pressure, previous_pressure, leakage_rate;
-unsigned long timmer = 0;
 
-bool start_pump_flag = 0, check_leak = 0, leakage_flag = 0;
+float current_pressure, previous_pressure;
 
 
-void MILLIS_TCB1_init(void);
+
+
+
 
 int main(void)
 {
   USART1_init(9600);
   Pressure_init();
   TCA0_PWM_init();
- 
- 
+  TCB_TIMER_init();
+  PORTE.DIR |= (1<<0); //PE0 VALVE 1
+  PORTE.DIR |= (1<<1); // PE1 VALVE 2
+  PORTE.OUTSET |= (1<<0);
+  PORTE.OUTSET |= (1<<1);
     while (1)
     {
 
 
  
-// USART1_sendString("leakage rate =");
- //USART1_sendFloat(current_pressure, 2);
- continous_intubation();
- //previous_pressure = current_pressure;
 
+ //USART1_sendFloat(current_pressure, 2);
+ if(!(PORTE.OUT && (1<<0)))
+{ continous_intubation();
+}
+else
+{TCA0.SINGLE.CMP0 = 0;
+}
+ //previous_pressure = current_pressure;
+USART1_sendInt(valve1_timer);
     }
 }
 
 void TCA0_PWM_init(void)
 {
-//sei();
+sei();
 /* First PWM on PA1 PIN */
 /* Direction set and set output to high */
 PORTA.DIR |= (1 << 0);
 PORTA.PIN0CTRL |= (1 << 3);
 //PORTA.OUTSET |= (1 << 0);
-TCA0.SINGLE.PER = 3999;//lculation for 1ms
+TCA0.SINGLE.PER = 3999;//calculation for 1ms
 
 TCA0.SINGLE.CNT = 0;
 
@@ -153,8 +151,7 @@ void continous_intubation(void)
 
 			P_control();
 			// USART1_sendFloat(current_pressure , 1);
-			PORTE_OUT &= ~(1 << 0);				// SOL 1 OFF
-			PORTE_OUT |= (1 << 1);					// SOL 2 ON
+			
 			TCA0.SINGLE.CTRLA |= (1 << 0);
 		//	USART1_sendString("Increasing Pressure");
 			TCA0.SINGLE.CMP0 = dutyCycle;			//MOTOR PWM
@@ -167,8 +164,6 @@ void continous_intubation(void)
 		current_pressure =  Pressure_read();
 		// USART1_sendFloat(current_pressure , 1);
 		P_control();
-		// PORTE_OUT |= (1 << 0);					// SOL 1 ON
-		PORTE_OUT &= ~(1 << 1);					// SOL 2 OFF
 		TCA0.SINGLE.CTRLA |= (1 << 0);
 		TCA0.SINGLE.CMP0 = 0;//dutyCycle;			//MOTOR PWM
 		
@@ -178,48 +173,41 @@ void continous_intubation(void)
 	{
 		current_pressure =  Pressure_read();
 		// USART1_sendFloat(current_pressure , 1);
-		PORTE_OUT &= ~(1 << 0);					// SOL 1 OFF
-		PORTE_OUT &= ~(1 << 1);					// SOL 2 OFF
-		//TCA0.SINGLE.CTRLA &= ~(1 << 0);		// MOTOR OFF
+        //TCA0.SINGLE.CTRLA &= ~(1 << 0);		// MOTOR OFF
 		dutyCycle = 0;
 		TCA0.SINGLE.CMP0 = dutyCycle;
 	}
 }
 
 
-void MILLIS_TCB1_init(void)
+void TCB_TIMER_init(void)
 {
-TCB1_CCMP = 3999; // Write a TOP value to the Compare/Capture (TCBn.CCMP) register
-
-TCB1_CTRLB |= (0x0 << 0);
-TCB1_INTCTRL |= (1<<0);
-
-TCB1_CTRLA |= (1<<0)|(0x0 <<1); // ENABLE bit in the Control A (TCBn.CTRLA) register,
+	TCB0.CCMP = 3906;
+	TCB0.CTRLA |= 0x05;
+	TCB0.INTCTRL |= (1<<0);
 }
 
 
-ISR(TCB1_INT_vect)
-{
-// to detect the rate of pressure decrease over time
-timmer++;
-if (timmer > 2000)
-{
-USART1_sendString("timmer");
-if (!(PORTE.IN & PIN0_bm) && !(PORTE.IN & PIN1_bm))
-{
-leakage_rate = previous_pressure - current_pressure;
-USART1_sendString("leakage rate =");
-USART1_sendFloat(leakage_rate, 2);
-if (leakage_rate > leakage_limit)
-{
-USART1_sendString("leakage detected");
-}
-}
-previous_pressure = current_pressure;
-timmer = 0;
-}
 
 
-TCB1_INTFLAGS |= (1<<0);
-
+ISR(TCB0_INT_vect)
+{   valve1_timer = valve1_timer+1;
+	if(valve1_timer <= 32000)
+	{
+		if(valve1_timer <= 16000)
+		{
+			PORTE.OUT |= (1<<0);
+		}
+		else
+		{
+			PORTE.OUT &=~ (1<<0);
+		}
+		
+	}
+	else
+	{
+		valve1_timer = 0;
+		PORTE_OUT ^= (1<<1); 
+	}
+	TCB0.INTFLAGS |= (1<<0);
 }
